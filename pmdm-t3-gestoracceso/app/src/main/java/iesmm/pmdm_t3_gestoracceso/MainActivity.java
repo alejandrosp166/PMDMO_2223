b8ae56f82d57cc346fa7f3faf592ace8426fcffa
alejandrosp166 <asecpin2303@iesmartinezm.es>
2022-11-07 20:22:39 +0100

    07/11/22

--- pmdm-t3-gestoracceso/app/src/main/java/iesmm/pmdm_t3_gestoracceso/MainActivity.java
+++ pmdm-t3-gestoracceso/app/src/main/java/iesmm/pmdm_t3_gestoracceso/MainActivity.java
@@ -11,50 +11,50 @@ import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Gravity;
-import android.widget.CalendarView;
+import android.view.View;
 import android.widget.LinearLayout;
-import android.widget.TableLayout;
 import android.widget.TextView;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.util.ArrayList;
 
 @RequiresApi(api = Build.VERSION_CODES.N)
 public class MainActivity extends AppCompatActivity {
     private final String LOGTAG = "PMDM";
     private final String FILENAME = "accesos.dat";
-    private Calendar calendario = new GregorianCalendar();
+    // Instanciamos el objeto  calendario que nos permite adquirir fechas del sistema
+    private Calendar date = new GregorianCalendar();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Registramos la entrada
+        registrarEstado("ENTRADA");
+        // Lo cargamos en la tabla
         cargarEstados();
-
-        String hora = String.valueOf(calendario.get(Calendar.HOUR_OF_DAY) + ":" + String.valueOf(calendario.get(Calendar.MINUTE)));
-        String fecha = String.valueOf(calendario.get(Calendar.DATE)) + "/" + String.valueOf(calendario.get(Calendar.MONTH) + "/" + String.valueOf(calendario.get(Calendar.YEAR)));
-
-        registrarEstado("ENTRADA", fecha, hora);
     }
 
     @Override
     protected void onDestroy() {
-        // convertimos en cadena la hora
-        String hora = String.valueOf(calendario.get(Calendar.HOUR_OF_DAY) + ":" + String.valueOf(calendario.get(Calendar.MINUTE)));
-        // Convertimos en cadena la fecha
-        String fecha = String.valueOf(calendario.get(Calendar.DATE)) + "/" + String.valueOf(calendario.get(Calendar.MONTH) + "/" + String.valueOf(calendario.get(Calendar.YEAR)));
-        registrarEstado("SALIDA", fecha, hora);
         super.onDestroy();
+        // Registramos la salida
+        registrarEstado("SALIDA");
     }
 
-    private void registrarEstado(String estado, String fecha, String hora) {
-        Log.i(LOGTAG, "Se registra la entrada");
+    /**
+     * Guarda el estado que se acaba de realizar puede ser o ENTRADA O SALIDA
+     *
+     * @param estado estado que se acaba de realizar
+     */
+    private void registrarEstado(String estado) {
+
+        // convertimos en cadena la hora
+        String hora = String.valueOf(date.get(Calendar.HOUR_OF_DAY) + ":" + String.valueOf(date.get(Calendar.MINUTE)));
+        // Convertimos en cadena la fecha
+        String fecha = String.valueOf(date.get(Calendar.DATE)) + " / " + String.valueOf(date.get(Calendar.MONTH) + " / " + String.valueOf(date.get(Calendar.YEAR)));
 
         try {
             // Crear el flujo de escritura
@@ -63,48 +63,68 @@ public class MainActivity extends AppCompatActivity {
             write.writeUTF(estado + "," + fecha + "," + hora);
             // Cerrar el flujo
             write.close();
-
+            // Mostramos en el log que se ha registrado una entrada a la app
+            Log.i(LOGTAG, "Se registra la " + estado + " en la FECHA " + fecha + " en la HORA " + hora);
         } catch (FileNotFoundException e) {
+            // Controlamos la excepción en el caso de que no se encuetre el fichero
             Log.e(LOGTAG, "Fichero no encontrado");
         } catch (IOException e) {
+            // Controlamos la excepión por si el flujo de lectura falla
             Log.e(LOGTAG, "Error en el flujo E/S");
         } catch (Exception e) {
+            // Controlamos las excepciones inesperadas
             Log.e(LOGTAG, "Error general");
         }
     }
 
+    /**
+     * Carga los estados anteriores en el caso de que los haya
+     */
     private void cargarEstados() {
-
         try {
+            // Creamos el flujo de lectura
             DataInputStream leer = new DataInputStream(this.openFileInput(FILENAME));
-            String l = leer.readLine();
+            // Guardamos la primera línea
+            String l = leer.readUTF();
+            // Recorremos las líneas del fichero mientras esta no sea null
             while (l != null) {
-
-                String[] temp = l.split(",");
-                mostrarBox(temp[0] + " " +temp[1] +" "+ temp[2]);
+                // Lo mostramos en la tabla
+                mostrarBox(l);
+                // Pasamos a la siguiente línea
+                l = leer.readUTF();
             }
-
-
+            // Mostramos en el log que se ha registrado una salida a la app
+            Log.i(LOGTAG, "Los datos se han cargado con éxito en la tabla");
         } catch (IOException e) {
+            // Controlamos la excepión por si el flujo de lectura falla
             Log.e(LOGTAG, "Error en el flujo E/S");
-       // } catch (ClassNotFoundException e) {
-            Log.e(LOGTAG, "Error, no se encontró la clase");
         } catch (Exception e) {
+            // Controlamos las excepciones inesperadas
             Log.e(LOGTAG, "Error general");
         }
     }
 
+    /**
+     * Formatea la salida en una tabla de datos
+     *
+     * @param cad es la cadena que se compone del estado, la fecha y la hora
+     */
     private void mostrarBox(String cad) {
         // Identificar el contenedor del layout
+        String[] temp = cad.split(",");
         LinearLayout layout = this.findViewById(R.id.container);
-        // Peronalizar el objeto textView
-        TextView box = new TextView(this);
-        box.setPadding(10, 20, 10, 10);
-        box.setGravity(Gravity.CENTER);
-        box.setTextColor(Color.RED);
-        box.setText(cad);
+        View box = new View(this);
 
-        //Agregar TextView al contenedor
+        for (int i = 0; i < 3; i++) {
+            TextView celda = new TextView(this);
+            // Peronalizar el objeto textView
+            celda.setPadding(10, 20, 10, 10);
+            celda.setGravity(Gravity.CENTER);
+            celda.setTextColor(Color.RED);
+            celda.setText(temp[i]);
+            //Agregar TextView al contenedor
+            
+        }
         layout.addView(box);
     }
 }
